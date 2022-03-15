0d332f8d4d7ee8e772405ba3e6a0956356dfa8cc
cvsantan <cvsantan@calpoly.edu>
2022-03-15 13:52:41 -0700

    update

--- src/mainpage.py
+++ src/mainpage.py
@@ -7,100 +7,6 @@
 
 @mainpage
 
-@section sec_1  HARDWARE DESIGN
-                This section describes the mechanisms, mechanical design, and
-                manufacturing techniques for each component of our 2.5D plotter.
-                Each subsection below describes a different component of the design,
-                explaining how and why we made the decisions that we did.
-                
-@subsection 1s1 
-                ##GENERAL IDEA
-                Our interpretation of a 2.5D plotter is a pancake plotter: a mechanism
-                that can convert G-Code of an image to carefully-positioned pancake
-                batter deposited onto a hot griddle, ideally making a pancake in the
-                shape of the chosen image.
-                
-                Our team plans to accomplish this using three axes of motion: one
-                rotational, one transverse, and one
-                also-transverse-but-it's-for-dispensing-batter. The finished mechanism
-                almost resembles a windshield wiper with a nozzle that moves along the
-                length of the blade, allowing us to cover a wide area with two motors.
-                A third motor will dispense the batter using a lead screw for precise
-                depositing. A GIF of the mechanism can be seen below.
-                
-                ## FULL MECH GIF
-                
-                It should be noted that the rotational-transverse design
-                is a challenge: surely a Cartesian (X-Y) plotter would be simpler. But
-                hey, we're Cal Poly MEs; we can take the challenge.
-                
-                A Bill of Materials (BOM) can be found at the end of this section, listing
-                names, suppliers, quantities, and costs for each purchased or designed item.
-                
-                Additionally, all CAD files can be found in the /docs/SOLIDWORKS directory
-                of this repository.
-                
-@subsection 1s2 
-                ##GRIDDLE
-                This is the most straight-forward component. In order to make pancakes,
-                you need a griddle or other hot-implement. Our team chose the griddle below,
-                purchased from Amazon for $27, due to its reasonable price and fast shipping
-                time (did I mention this project only took four weeks?).
-                
-                ## GRIDDLE IMAGE
-                
-                There's really not much else for this one. It's a griddle.
-
-@subsection 1s3 
-                ##SUPPORT STRUCTURE
-                In order to effectively position and control our motors, our design first
-                needed a support structure. We designed the base of the support structure
-                from aluminum extrusions due to their versatility (used for both the
-                supports and the actuating rail!), low cost, and fast shipping. Our team
-                purchased L-shaped brackets that fit within the extrusion rails for securing
-                multiple components to the support structure - these brackets were used in
-                almost every component of the design. We were able to laser-cut a piece
-                of plywood to create our support rail that the mechanism rests and slides
-                on in our on-campus machine shops. Lastly, we designed and 3D-printed some
-                inserts / risers that elevated the support rail above the griddle. The risers
-                were printed in PLA and are positioned far enough from the heating element
-                to reduce the risk of heat deformation.
-                
-                ## SUPPORT STRUCTURE IMAGE
-                
-                Purchasing aluminum extrusions and L-brackets, laser-cutting the plywood
-                on-campus, and 3D-printing the risers allowed us to keep costs low,
-                reduced our manufacturing time (as opposed to machining metal), and
-                allowed for rapid prototyping and design changes (of which there were many)!
-
-@subsection 1s4 
-                ##BATTER DISPENSER
-                Time for moving parts!
-                
-                ## BATTER DISPENSER GIF
-                
-                This component is the "0.5" axis of our 2.5 axis plotter. It dispenses batter
-                at a constant rate when told to by the microcontroller, and is made up of a
-                syringe for holding batter, an endstop for securing the syringe, a plunger
-                mechanism for pushing the batter, a motor and lead screw for creating linear
-                dispensing motion, and a PVC tube for moving batter to the nozzle holder.
-                
-                ## LABELED DISPENSER IMAGE
-
-@subsection 1s5 
-                ##TRANSVERSE MOTION
-
-@subsection 1s6 
-                ##ROTATIONAL MOTION
-
-@subsection 1s7 
-                ##LIMIT SWITCHES AND "ZEROING"
-
-@subsection 1s8 
-                ##ELECTRONICS HOUSING
-
-@subsection 1s9 
-                ##BILL OF MATERIALS
 
 @section sec_2  SOFTWARE DESIGN
                 This section will cover the tasks that we plan on utilizing for
@@ -114,8 +20,30 @@
                 
                 ## Include picture of task diagram
                 
-
-@subsection 2s1 
+@subsection 2s1
+                ##RADIAL LIMIT SWITCH TASK
+                The radial limit switch task continuouslly checks to see if the 
+                limit switch for the radial motor has been actuated. This is 
+                accomplished through the use of a shared flag variable. When the
+                task is initialized the value of this shared variable is set to 
+                zero. When the limit switch is finally actuated, the value of 
+                the shared variable is changed to one, this is then used to start
+                the plotting portion for the radial motor task.
+                
+@subsection 2s2
+                ##TRANSVERSE LIMIT SWITCH TASK
+                The transverse limit switch task continuouslly checks to see if the 
+                limit switch for the transverse motor has been actuated. This is 
+                accomplished through the use of a shared flag variable. When the
+                task is initialized the value of this shared variable is set to 
+                zero. When the limit switch is finally actuated, the value of 
+                the shared variable is changed to one, this is then used to start
+                the plotting portion for the transverse motor task.
+@subsection 2s3
+                ##EXTRUSION LIMIT SWITCH TASK
+                The extrusion limit switch task continuously checks to see if the 
+                limit switch for the extrusion motor task has benn actuated.
+@subsection 2s14
                 ##G-CODE TASK
                 The G-Code task will be responsible for the conversion a G-Code
                 into coordinates that can be used by our machine. This will be
@@ -128,40 +56,58 @@
                 
                 ## FSM?
                 
-@subsection 2s2 
+@subsection 2s5 
                 ##RADIAL MOTOR TASK
                 The radial motor task will be responsible for the radial motion
-                of our nozzle. This task will function by taking the radial
-                coordinates from the G-Code task and using these values as refernece
-                point for a controller object. The output of the controller will
-                then be used to apply a duty cycle to the radial motor. Since we
-                will be utilizing the encoder to measure position, it will be
-                important to convert the rotational motion of the encoder to a 
-                linear distance that we can compare the radial coordinates against.
-                Once the desired coordinate is reached, a flag variable will be sent
-                out to the syringe motor task indicating it is ready for pancake
-                mix to be dispersed.
+                of our nozzle. In order to accomplish this the radial motion will
+                first locate itself using the limit switch located on its path.
+                When the task first runs the radial motor will move the arm 
+                slightly away from, and then towards the limit switch until it
+                makes contact with it. Once the arm makes contact with the limit
+                switch, the position of the encoder is updated to correspond
+                with the the position of the limit switch. It will also wait 
+                for the transverese motor to make contact with its limit switch 
+                before proceeding. Once both the radial and transverse limit 
+                switches have been actauted, the radial task will begin to make  
+                its way through a set of coordinates in a list moving the motor 
+                to the specified list index. The radial task willnot move onto 
+                the next index until both the radial and transverse coordinates 
+                of the same index have both been reached. A constant duty cycle
+                is appied to the motor as it goes through the list, and it
+                utilizes PI positional control to make sure it gets as close to
+                the specified coordinates as it can.
                 
                 ## FSM?
                 
-@subsection 2s3 
+@subsection 2s6
                 ##TRANSVERSE MOTOR TASK
                 The transverse motor task will be responsible for the transverse
-                motion of our nozzle. This task will function by taking the 
-                transverse coorinates coordinates from the G-Code task and using
-                these values as reference points for a controller object. The output
-                of the controller object will then be used to apply a duty cycle
-                toi the transverse motor. Since we will be using the encoders to 
-                measure position, we need to make sure that the encoder reads
-                degrees and not ticks. Once the desired coordinate is reached,
-                a flag variable will be sent out to the syringe motor task
-                indicating it is ready for pancake mix to be dispersed.
+                motion of our nozzle. Its functionality is very similar to that
+                of the radial motor task; however, since this task  The task begins by first locating 
+                itself using the limit switch located on its path by mocing the
+                nozzle toward the limit switch located at the end of the radial arm
+                until it makes contact. Once it makes contact the position of the 
+                transverse encoder is updated to match the position of the limit switch.
+                It will also wait for the radial motor to make contact with its 
+                limit switch until before procedding.Once both the radial and 
+                transverse limit  switches have been actauted, the transverse task 
+                will begin to make its way through a set of coordinates in a list,
+                moving the motor to the specified list index. The transverse task 
+                will not move onto the next index until both the radial and 
+                transverse coordinates of the same index have both been reached. 
+                A constant duty cycle is appied to the motor as it goes through
+                the list, and it utilizes PI control to make sure t gets as close
+                to the specified coordinates as it can. Also, since this task tries to
+                measure linear distance using angular readings, the encoder values 
+                must be converted to linear.
                 
+
                 ## FSM?
                 
-@subsection 2s4 
-                ##SYRINGE MOTOR TASK
+@subsection 2s7 
+                ##EXTRUSION MOTOR TASK
                 The syringe motor task will be responsible for the dispersion
                 of the pancake mixture.
+
 '''
 
